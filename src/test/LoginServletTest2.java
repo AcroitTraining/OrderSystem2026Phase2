cf065982272e06286614f686fa24d86bade779a9
秋嶋琉太 <@AC2026-80-002>
2026-07-10 10:28:37 +0900

    push up

---
+++ src/test/LoginServletTest2.java
@@ -0,0 +1,46 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mock;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.LoginInfo;
+
+@ExtendWith(MockitoExtension.class)
+class LoginServletTest2 {
+	// @Mock private OrderStartDAO dao;
+	@Mock private HttpServletRequest request;
+	@Mock private HttpServletResponse response;
+	@Mock private RequestDispatcher dispatcher;
+
+	@Test
+	void testLoginServlet() throws ServletException, IOException, SQLException, ClassNotFoundException {
+
+		String userId = "order";
+		String password = "1234";
+		LoginInfo InfoTest = new LoginInfo(userId, password);
+
+		when(request.getParameter("userId")).thenReturn("order");
+		when(request.getParameter("password")).thenReturn("1234");
+		doNothing().when(dispatcher).forward(request, response);
+		when(request.getRequestDispatcher("HomeServlet")).thenReturn(dispatcher);
+
+		// servletの実行
+		servlet.doPost(request, response);
+
+		LoginInfo Info = servlet.getLoginInfo();
+
+		asssertEquals(InfoTest.getLoginId(), Info.getLoginId());
+		asssertEquals(InfoTest.getLoginPassword(), Info.getLoginPassword());
+
+		verify(request, times(1)).setAttribute("loginInfo", Info);
+		verify(request, times(1)).getRequestDispatcher(anyString());
+
+	}
